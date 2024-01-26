e14ef4b4e49ce3960709ef7372cfbc1af3a17f40
Georgi Tsankov <145467172+GeorgiGeorgievTsankov@users.noreply.github.com>
2024-01-26 16:57:33 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,44 @@
+
+const App = () => {
+  return (
+    <div className="categories-container">
+      <div className="category-container">
+        {/* <img src=""></img> */}
+        <div className="category-body-container">
+          <h2>Hats</h2>
+          <p>Shop now</p>
+        </div>
+      </div>
+      <div className="category-container">
+        {/* <img src=""></img> */}
+        <div className="category-body-container">
+          <h2>Jackets</h2>
+          <p>Shop now</p>
+        </div>
+      </div>
+      <div className="category-container">
+        {/* <img src=""></img> */}
+        <div className="category-body-container">
+          <h2>Sneakers</h2>
+          <p>Shop now</p>
+        </div>
+      </div>
+      <div className="category-container">
+        {/* <img src=""></img> */}
+        <div className="category-body-container">
+          <h2>Womans</h2>
+          <p>Shop now</p>
+        </div>
+      </div>
+      <div className="category-container">
+        {/* <img src=""></img> */}
+        <div className="category-body-container">
+          <h2>Mens</h2>
+          <p>Shop now</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
