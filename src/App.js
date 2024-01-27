bb6d2b6871b2cced37c187f789858bf77f0f683b
Georgi Tsankov <145467172+GeorgiGeorgievTsankov@users.noreply.github.com>
2024-01-27 16:25:32 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,53 +1,39 @@
+
 import './App.css'
+import { DirectoryComponent } from './Components/Directory/DirectoryComponent';
 
 
 const App = () => {
 
   const categories = [
     {
-      "id": "1",
+      "id": 1,
       "title": "hats",
       "imageUrl": "https://i.ibb.co/cvpntL1/hats.png"
     },
     {
-      "id": "2",
+      "id": 2,
       "title": "jackets",
       "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
     },
     {
-      "id": "3",
+      "id": 3,
       "title": "sneakers",
       "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
     },
     {
-      "id": "4",
+      "id": 4,
       "title": "womans",
       "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
     },
     {
-      "id": "5",
+      "id": 5,
       "title": "mens",
       "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
     }
   ]
 
-  
-  return (
-    <div className="categories-container">
-      {categories.map(({ id,title,imageUrl }) =>(
-          <div key={id} className="category-container">
-          <div className="background-image" style={{ backgroundImage: `url(${imageUrl})` }}>
-            <div className="category-body-container">
-              <h2>{title}</h2>
-              <p>Shop now</p>
-            </div>
-          </div>
-        </div>
-      ))};
-
-    </div>
-
-  )
+  return (<DirectoryComponent categories={categories}/>)
 
 }
 
