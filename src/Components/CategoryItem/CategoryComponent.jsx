7f45600d23f9ea87a9a6d4a2e891d660499309e6
Georgi Tsankov <145467172+GeorgiGeorgievTsankov@users.noreply.github.com>
2024-01-27 16:29:02 +0200

    Update CategoryComponent.jsx

--- src/Components/CategoryItem/CategoryComponent.jsx
+++ src/Components/CategoryItem/CategoryComponent.jsx
@@ -1,7 +1,19 @@
 import React from 'react'
+import './CategoryComponent.css'
 
-export const CategoryComponent = () => {
-  return (
-    <div>CategoryComponent</div>
-  )
+
+export const CategoryComponent = ({ category }) => {
+    const { imageUrl, title } = category;
+
+    return (
+        <div className="category-container">
+            <div className="background-image" style={{ backgroundImage: `url(${imageUrl})` }}>
+                <div className="category-body-container">
+                    <h2>{title}</h2>
+                    <p>Shop now</p>
+                </div>
+            </div>
+        </div>
+    )
 }
+
