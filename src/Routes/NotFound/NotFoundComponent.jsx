abad720433e93c9f322ad7f9e87df0c678196776
Georgi Tsankov <145467172+GeorgiGeorgievTsankov@users.noreply.github.com>
2024-01-28 15:04:14 +0200

    Add files via upload

---
+++ src/Routes/NotFound/NotFoundComponent.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export const NotFoundComponent = () => {
+  return (
+    <div className='noFound'>
+        <h1>The page is not found</h1>
+        <Link to={"/home"}><p>Click here to homepage...</p></Link>
+    </div>
+  )
+}
