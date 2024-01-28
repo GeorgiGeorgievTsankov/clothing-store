159aa18bf2945dd656865a7c506ccaaaed4ae1f3
Georgi Tsankov <145467172+GeorgiGeorgievTsankov@users.noreply.github.com>
2024-01-28 15:44:10 +0200

    Update NavigationComponent.jsx

--- src/Routes/Navigation/NavigationComponent.jsx
+++ src/Routes/Navigation/NavigationComponent.jsx
@@ -1,7 +1,22 @@
 import React from 'react'
+import { Link } from 'react-router-dom/cjs/react-router-dom'
+import { ReactComponent as CrwnLogo } from '../../Components/Assets/crown.svg'
+import './NavigationCSS.css'
 
 export const NavigationComponent = () => {
   return (
-    <div>NavigationComponent</div>
+      <div className='navigation'>
+        <Link className='logo-container' to='/'>
+          <CrwnLogo className='logo' />
+        </Link>
+        <div className='nav-links-container'>
+          <Link className='nav-link' to='/shop'>
+            SHOP
+          </Link>
+          <Link className='nav-link' to='/sign-in'>
+            SIGN IN
+          </Link>
+        </div>
+      </div>
   )
 }
