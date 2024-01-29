73095ab1552fb4da623291523036eb5f6546537b
Georgi Tsankov <145467172+GeorgiGeorgievTsankov@users.noreply.github.com>
2024-01-29 18:34:53 +0200

    Update SignInComponent.jsx

--- src/Routes/Sign-In/SignInComponent.jsx
+++ src/Routes/Sign-In/SignInComponent.jsx
@@ -1,7 +1,31 @@
 import React from 'react'
+import { signInWithGooglePopup, createUserDocumentFromAuth } from '../../Utils/Firebase/Firebase.utils'
 
 export const SignInComponent = () => {
-  return (
-    <div>SignInComponent</div>
-  )
+
+    const logGoogleUser = async () => {
+        try {
+            const user = await signInWithGooglePopup();
+            console.log("Logged in user:", user);
+    
+            if (user) {
+                const userDocRef = await createUserDocumentFromAuth(user);
+                console.log("User document reference:", userDocRef);
+            }
+        } catch (error) {
+            console.error("Error logging in with Google:", error.message);
+        }
+    };
+    
+
+
+    return (
+        <div>
+            <h1>
+                SignIn
+            </h1>
+            <button onClick={logGoogleUser}>Sing In with googlePopup</button>
+        </div>
+
+    )
 }
