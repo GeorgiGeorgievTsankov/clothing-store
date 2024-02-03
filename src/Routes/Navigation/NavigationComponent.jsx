7d4fd2dcc3d4ea5de454801da2b204b97d4ac716
Georgi Tsankov <145467172+GeorgiGeorgievTsankov@users.noreply.github.com>
2024-02-03 15:10:56 +0200

    Update NavigationComponent.jsx

--- src/Routes/Navigation/NavigationComponent.jsx
+++ src/Routes/Navigation/NavigationComponent.jsx
@@ -2,21 +2,27 @@ import React from 'react'
 import { Link } from 'react-router-dom/cjs/react-router-dom'
 import { ReactComponent as CrwnLogo } from '../../Components/Assets/crown.svg'
 import './NavigationCSS.css'
+import { useContext } from 'react'
+import { UserContext } from '../../Contexts/UserContext'
+
 
 export const NavigationComponent = () => {
+
+  const { currentUser } = useContext(UserContext);
+
   return (
-      <div className='navigation'>
-        <Link className='logo-container' to='/'>
-          <CrwnLogo className='logo' />
+    <div className='navigation'>
+      <Link className='logo-container' to='/'>
+        <CrwnLogo className='logo' />
+      </Link>
+      <div className='nav-links-container'>
+        <Link className='nav-link' to='/shop'>
+          SHOP
         </Link>
-        <div className='nav-links-container'>
-          <Link className='nav-link' to='/shop'>
-            SHOP
-          </Link>
-          <Link className='nav-link' to='/sign-in'>
-            SIGN IN
-          </Link>
-        </div>
+        <Link className='nav-link' to='/sign-in'>
+          SIGN IN
+        </Link>
       </div>
+    </div>
   )
 }
