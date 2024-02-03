21ce12c62ab5408eeef564193caca4d74c981b48
Georgi Tsankov <145467172+GeorgiGeorgievTsankov@users.noreply.github.com>
2024-02-03 15:08:06 +0200

    Update UserContext.jsx

--- src/Contexts/UserContext.jsx
+++ src/Contexts/UserContext.jsx
@@ -2,7 +2,7 @@ import { createContext, useState } from 'react'
 
 export const UserContext = createContext({
     currentUser:null,
-    setCurrentUser: () => nill,
+    setCurrentUser: () => null,
 })
 
 export const UserProvider = ({ children }) => {
