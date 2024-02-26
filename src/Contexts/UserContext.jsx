be6ced3102407a53d0de9631edbeb587e3f63b93
Georgi Tsankov <145467172+GeorgiGeorgievTsankov@users.noreply.github.com>
2024-02-26 18:47:03 +0200

    Update UserContext.jsx

--- src/Contexts/UserContext.jsx
+++ src/Contexts/UserContext.jsx
@@ -1,12 +1,22 @@
-import { createContext, useState } from 'react'
+import { createContext, useState, useEffect } from 'react'
+import { onAuthStateChangedListener } from '../Utils/Firebase/Firebase.utils'
 
 export const UserContext = createContext({
-    currentUser:null,
+    currentUser: null,
     setCurrentUser: () => null,
 })
 
 export const UserProvider = ({ children }) => {
     const [currentUser, setCurrentUser] = useState(null);
-    const value = { currentUser: currentUser, setCurrentUser};
-    return <UserContext.Provider value={ value } >{children}</UserContext.Provider>
+    const value = { currentUser: currentUser, setCurrentUser };
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChangedListener((user) => {
+            console.log(user);
+        })
+
+        return unsubscribe
+    }, [])
+
+    return <UserContext.Provider value={value} >{children}</UserContext.Provider>
 }
