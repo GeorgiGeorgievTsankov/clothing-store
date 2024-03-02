5639304b85875d59ca6aa47fd48f2e453b3b05d2
Georgi Tsankov <145467172+GeorgiGeorgievTsankov@users.noreply.github.com>
2024-03-02 14:41:35 +0200

    Update ProductsContext.jsx

--- src/Contexts/ProductsContext.jsx
+++ src/Contexts/ProductsContext.jsx
@@ -1,17 +1,17 @@
-import { createContext, useState } from 'react';
+import { createContext,useState,useEffect } from "react";
+import PRODUCTS from "../shop-data.json";
 
-import PRODUCTS from '../shop-data.json';
 
-export const ProductsContext = createContext({
-  products: [],
-});
+export const ProductsContext = () => {
+ products : [];
+}
 
-export const ProductsProvider = ({ children }) => {
-  const [products, setProducts] = useState(PRODUCTS);
-  const value = { products };
-  return (
-    <ProductsContext.Provider value={value}>
-      {children}
-    </ProductsContext.Provider>
-  );
-};
+export const ProductsProvider = ({children}) =>{
+
+    const [products, setProducts] = useState(PRODUCTS);
+    const value = {products};
+    
+    return (
+        <ProductsContext.Provider value={value}>{ children }</ProductsContext.Provider>
+    )
+}
