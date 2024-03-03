f465d309c921edfec7c1bc41a9598e111460e1d8
Georgi Tsankov <145467172+GeorgiGeorgievTsankov@users.noreply.github.com>
2024-03-03 17:46:21 +0200

    Update NavigationComponent.jsx

--- src/Routes/Navigation/NavigationComponent.jsx
+++ src/Routes/Navigation/NavigationComponent.jsx
@@ -4,13 +4,18 @@ import { ReactComponent as CrwnLogo } from '../../Components/Assets/crown.svg'
 import './NavigationCSS.css'
 import { useContext } from 'react'
 import { UserContext } from '../../Contexts/UserContext'
+import { CartContext } from '../../Contexts/CartContext'
 import { singOutUser } from '../../Utils/Firebase/Firebase.utils'
+import { CartIconComponent } from '../../Components/CartIcon/CartIconComponent'
+import { CartDropdownComponent } from '../../Components/CartDropdown/CartDropdownComponent'
+
+
 
 
 export const NavigationComponent = () => {
 
   const { currentUser } = useContext(UserContext);
-  
+  const { isCartOpen } = useContext(CartContext);
 
   return (
     <div className='navigation'>
@@ -30,9 +35,11 @@ export const NavigationComponent = () => {
             </Link>
             )
         }
-
-      </div>
+        <CartIconComponent />
+      </div>     
+      {
+        isCartOpen ? <CartDropdownComponent /> : ""
+      }
     </div>
   )
 }
-
