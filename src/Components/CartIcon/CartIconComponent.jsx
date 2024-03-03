6859f1a71e8cec7b3cc4785dcb27b427eca32d2f
Georgi Tsankov <145467172+GeorgiGeorgievTsankov@users.noreply.github.com>
2024-03-03 17:05:08 +0200

    Add files via upload

---
+++ src/Components/CartIcon/CartIconComponent.jsx
@@ -0,0 +1,21 @@
+// import { useContext } from 'react';
+
+import { ReactComponent as ShoppingIcon } from '../Assets/shopping-bag.svg';
+
+// import { CartContext } from '../../Contexts/CartContext';
+
+import './CartIconStyle.css';
+
+export const CartIconComponent = () => {
+  // const { isCartOpen, setIsCartOpen } = useContext(CartContext);
+
+  // const toggleIsCartOpen = () => setIsCartOpen(!isCartOpen);
+
+  return (
+    <div className='cart-icon-container' >
+      <ShoppingIcon className='shopping-icon' />
+      <span className='item-count'>0</span>
+    </div>
+  );
+};
+
