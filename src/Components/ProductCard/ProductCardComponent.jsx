d654b94fde8bb6069253138ec788ef74057fb046
Georgi Tsankov <145467172+GeorgiGeorgievTsankov@users.noreply.github.com>
2024-03-03 15:49:23 +0200

    Update ProductCardComponent.jsx

--- src/Components/ProductCard/ProductCardComponent.jsx
+++ src/Components/ProductCard/ProductCardComponent.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import './ProductCardStyles.css';
 
-export const ProductCardComponent = () => {
+import Button from '../Button/ButtonComponent';
+
+const ProductCardComponent = ({ product }) => {
+  const { name, price, imageUrl } = product;
   return (
-    <div>ProductCardComponent</div>
-  )
-}
+    <div className='product-card-container'>
+      <img src={imageUrl} alt={`${name}`} />
+      <div className='footer'>
+        <span className='name'>{name}</span>
+        <span className='price'>{price}</span>
+      </div>
+      <Button buttonType='inverted'>Add to card</Button>
+    </div>
+  );
+};
+
+export default ProductCard;
