9cdad57f097ee92ae8769921245deec255f937b1
Georgi Tsankov <145467172+GeorgiGeorgievTsankov@users.noreply.github.com>
2024-03-07 15:31:34 +0200

    Update CartContext.jsx

--- src/Contexts/CartContext.jsx
+++ src/Contexts/CartContext.jsx
@@ -4,10 +4,18 @@ import { createContext, useState } from 'react';
 export const CartContext = createContext({
   isCartOpen: false,
   setIsOpen: () => {},
+  cartItem : [],
+  addItemToCart: () => {},
 });
 
 export const CartProvider = ({ children }) => {
   const [isCartOpen, setIsCartOpen] = useState(false);
+  const [cartItem, setCartItems] = useState([]);
+
+    const addItemToCart = (productToAdd) => {
+        
+    }
+
   const value = { isCartOpen, setIsCartOpen };
   return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
 };
