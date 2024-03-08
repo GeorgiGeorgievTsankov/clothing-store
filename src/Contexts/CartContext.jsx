3d8ce2241a1a390f99aa7d7e8afb3c472c1c7702
Georgi Tsankov <145467172+GeorgiGeorgievTsankov@users.noreply.github.com>
2024-03-08 18:58:08 +0200

    Update CartContext.jsx

--- src/Contexts/CartContext.jsx
+++ src/Contexts/CartContext.jsx
@@ -1,4 +1,4 @@
-
+import { useEffect } from 'react';
 import { createContext, useState } from 'react';
 
 export const addCartItem = (cartItems, productToAdd) => {
@@ -23,16 +23,26 @@ export const CartContext = createContext({
   setIsOpen: () => {},
   cartItems : [],
   addItemToCart: () => {},
+  cartItemCount: 0,
 });
 
 export const CartProvider = ({ children }) => {
   const [isCartOpen, setIsCartOpen] = useState(false);
   const [cartItems, setCartItems] = useState([]);
+  const [cartItemCount, setCartItemCount] = useState(0);
+
+  useEffect(() => {
+    const count = cartItems.reduce(
+      (total, cartItem) => total + cartItem.quantity,
+      0
+    );
+    setCartItemCount(count);
+  }, [cartItems]);
 
     const addItemToCart = (productToAdd) => {
         setCartItems(addCartItem(cartItems, productToAdd));
     }
 
-  const value = { isCartOpen, setIsCartOpen, addItemToCart, cartItems};
+  const value = { isCartOpen, setIsCartOpen, addItemToCart, cartItems, cartItemCount};
   return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
 };
