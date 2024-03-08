c11350db674aaca09665b7233e62a2c92706cbff
Georgi Tsankov <145467172+GeorgiGeorgievTsankov@users.noreply.github.com>
2024-03-08 18:57:25 +0200

    Update CartIconComponent.jsx

--- src/Components/CartIcon/CartIconComponent.jsx
+++ src/Components/CartIcon/CartIconComponent.jsx
@@ -1,5 +1,6 @@
 import { useContext } from 'react';
 
+
 import { ReactComponent as ShoppingIcon } from '../Assets/shopping-bag.svg';
 
 import { CartContext } from '../../Contexts/CartContext';
@@ -8,15 +9,18 @@ import './CartIconStyle.css';
 
 export const CartIconComponent = () => {
   const { isCartOpen, setIsCartOpen } = useContext(CartContext);
-
+  const { cartItemCount } = useContext(CartContext);
+  
+  
   const toggleIsCartOpen = () => setIsCartOpen(!isCartOpen);
-
+  
   return (
     <div className='cart-icon-container' onClick={ toggleIsCartOpen } >
-      <ShoppingIcon className='shopping-icon' />
-      <span className='item-count'>0</span>
+    <ShoppingIcon className='shopping-icon' />
+    <span className='item-count'>{ cartItemCount }</span>
     </div>
-  );
-};
+    );
+  };
+  
 
 
