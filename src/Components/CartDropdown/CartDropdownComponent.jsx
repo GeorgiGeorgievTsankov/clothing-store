f798756d61dc311be4888078010468bde0c8bdf1
Georgi Tsankov <145467172+GeorgiGeorgievTsankov@users.noreply.github.com>
2024-03-12 14:47:18 +0200

    Update CartDropdownComponent.jsx

--- src/Components/CartDropdown/CartDropdownComponent.jsx
+++ src/Components/CartDropdown/CartDropdownComponent.jsx
@@ -4,6 +4,7 @@ import { ButtonComponent } from '../Button/ButtonComponent';
 import { useContext } from 'react';
 import { CartContext } from '../../Contexts/CartContext';
 import { useHistory } from 'react-router-dom/cjs/react-router-dom';
+// import {useNavigation } from 'react-router'  
 
 
 
@@ -14,12 +15,18 @@ export const CartDropdownComponent = () => {
     const { isCartOpen, setIsCartOpen } = useContext(CartContext);
     const history = useHistory();
 
-    const handleClick = () => {
+    const goToCheckoutHandler = () => {
         history.push('/checkout');
         setIsCartOpen(false);
-
-
     }
+
+    //Друг начин за навигиране 
+    //const navigate = useNavigate();
+    // const goToCheckoutHandler = () => {
+    //     navigate('/checkout');
+    //   };
+
+
 
     const {cartItems } = useContext(CartContext);
 
@@ -29,7 +36,7 @@ export const CartDropdownComponent = () => {
                 {cartItems.map(item => <CartItemComponent key={item.id} cartItem={item} />)}
             </div>
 
-            <ButtonComponent onClick={handleClick}>GO TO CHECKOUT</ButtonComponent>
+            <ButtonComponent onClick={goToCheckoutHandler}>GO TO CHECKOUT</ButtonComponent>
         </div>
     )
 
