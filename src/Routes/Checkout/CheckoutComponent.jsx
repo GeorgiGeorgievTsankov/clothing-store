1f05ab70092c95e203a4c76e1ec573f1f71bed2e
Georgi Tsankov <145467172+GeorgiGeorgievTsankov@users.noreply.github.com>
2024-03-12 15:22:43 +0200

    Add files via upload

---
+++ src/Routes/Checkout/CheckoutComponent.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { useContext } from 'react'
+import './CheckoutStyle.css'
+import { CartContext, addCartItem } from '../../Contexts/CartContext'
+
+
+
+export const CheckoutComponent = () => {
+
+    const { cartItems, addItemToCart } = useContext(CartContext);
+
+
+    return (
+      <div>
+        <h1>Hi Checkout</h1>
+        <div>
+            {cartItems.map((cartItem) => {
+                const { name, id, quantity } = cartItem;
+                return(
+                    <div key={id}>
+                        <h2>{name}</h2>
+                        <span>{quantity}</span>
+                        <span>decrement</span>
+                        <span onClick={() => addItemToCart(cartItem)}>increment</span>
+                    </div>
+                )
+
+            })}
+        </div>
+      </div>
+    );
+
+}
