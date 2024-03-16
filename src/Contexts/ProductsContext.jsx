b762ff9c61baaab2445b63eb9878467e691ea8e0
Georgi Tsankov <145467172+GeorgiGeorgievTsankov@users.noreply.github.com>
2024-03-16 17:24:00 +0200

    Update ProductsContext.jsx

--- src/Contexts/ProductsContext.jsx
+++ src/Contexts/ProductsContext.jsx
@@ -1,15 +1,24 @@
 import { createContext, useState, useEffect } from "react";
-import SHOP_DATA from "../shop-data.js";
-import {addCollectionsAndDocuments} from '../Utils/Firebase/Firebase.utils.js'
+
+import { getCategoriesAndDocuments } from '../Utils/Firebase/Firebase.utils.js'
 
 export const ProductsContext = createContext({
-    products : [],
+    products: [],
 })
 
 export const ProductsProvider = ({ children }) => {
 
     const [products, setProducts] = useState([]);
- 
+    
+    useEffect(() => {
+        const getCategoriesMap = async () => {
+            const categoryMap = await getCategoriesAndDocuments();
+            console.log(categoryMap);
+        }
+        getCategoriesMap();
+
+    }, []);
+
     const value = { products };
 
     return (
