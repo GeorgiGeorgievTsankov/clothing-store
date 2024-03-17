fa18e0d3b2edaea11c1ce04159215becd2d0d9e3
Georgi Tsankov <145467172+GeorgiGeorgievTsankov@users.noreply.github.com>
2024-03-17 14:38:06 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom'
 import { UserProvider } from './Contexts/UserContext';
-import { ProductsProvider } from './Contexts/ProductsContext'; 
+import { CategoriesProvider } from './Contexts/ProductsContext'; 
 import { CartProvider } from './Contexts/CartContext';
 
 import './index.css';
@@ -15,14 +15,19 @@ root.render(
     <BrowserRouter>
       <UserProvider>
       <CartProvider>
-        <ProductsProvider>
+        <CategoriesProvider>
           <App />       
-        </ProductsProvider>
+        </CategoriesProvider>
         </CartProvider>
       </UserProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
