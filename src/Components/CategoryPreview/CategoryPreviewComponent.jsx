66a8b1504405aa7b2837655043072e646ac6037a
Georgi Tsankov <145467172+GeorgiGeorgievTsankov@users.noreply.github.com>
2024-03-18 18:36:49 +0200

    Update CategoryPreviewComponent.jsx

--- src/Components/CategoryPreview/CategoryPreviewComponent.jsx
+++ src/Components/CategoryPreview/CategoryPreviewComponent.jsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import { Link } from 'react-router-dom';
+import './CategoryPreviewStyle.css';
+import ProductCardComponent from '../ProductCard/ProductCardComponent'
 
-export const CategoryPreviewComponent = () => {
-  return (
-    <div>CategoryPreviewComponent</div>
-  )
+
+const CategoryPreviewComponent = ({ title, products }) => {
+    return (
+        <div className='category-preview-container'>
+          <h2>
+            <Link className='title' to={title}>
+              {title.toUpperCase()}
+            </Link>
+          </h2>
+          <div className='preview'>
+            {products
+              .filter((_, idx) => idx < 4)
+              .map((product) => (
+                <ProductCardComponent key={product.id} product={product} />
+              ))}
+          </div>
+        </div>
+      );
 }
+
+export default CategoryPreviewComponent
