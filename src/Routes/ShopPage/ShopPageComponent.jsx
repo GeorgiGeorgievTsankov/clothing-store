a75b19de67e72b385e2eba8dd0ccc8f3a8348f9d
Georgi Tsankov <145467172+GeorgiGeorgievTsankov@users.noreply.github.com>
2024-03-22 15:36:34 +0200

    Update ShopPageComponent.jsx

--- src/Routes/ShopPage/ShopPageComponent.jsx
+++ src/Routes/ShopPage/ShopPageComponent.jsx
@@ -1,20 +1,21 @@
-import { BrowserRouter as Router, Route} from 'react-router-dom';
-import { CategoriesPreviewComponent }  from '../CategoriesPreview/CategoriesPreviewComponent';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { CategoriesPreviewComponent } from '../CategoriesPreview/CategoriesPreviewComponent';
+import { CategoryComponent } from '../Category/CategoryComponent';
 
 import "../ShopPage/ShopPageStyle.css"
 
 
 
-export const ShopPageComponent = () => {
 
-   
-    
-    
+export const ShopPageComponent = () => {
     return (
+
         <Router>
-            <Route path="/">
-                <CategoriesPreviewComponent />
-            </Route>
+            <Route index element={<CategoriesPreviewComponent />} />
+            <Route path=':category' element={<CategoryComponent />} />
         </Router>
-    );
+
+    )
+
 }
+
