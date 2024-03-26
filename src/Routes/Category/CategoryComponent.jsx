fac457493c93b982df2d8fc55c9dced7e3a56030
Georgi Tsankov <145467172+GeorgiGeorgievTsankov@users.noreply.github.com>
2024-03-26 18:20:02 +0200

    Add files via upload

---
+++ src/Routes/Category/CategoryComponent.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { useContext,useEffect,useState } from 'react';
+import { CategoriesContext } from '../../Contexts/ProductsContext';
+import { useParams } from 'react-router-dom/cjs/react-router-dom';
+import "./CategoryStyle.css"
+import ProductCardComponent from '../../Components/ProductCard/ProductCardComponent';
+
+export const CategoryComponent = () => {
+
+  const {category} = useParams();
+  const {categoriesMap} = useContext(CategoriesContext)
+ const [products, setProducts] = useState(categoriesMap[category]);
+
+ useEffect(() => {
+  setProducts(categoriesMap[category]);
+ }, [categoriesMap, category])
+
+  return (
+  <div className='category-container'>
+     {products && products.map((product) => <ProductCardComponent key={product.id} product={product} />)}
+  </div>
+  )
+}
+
+
+
