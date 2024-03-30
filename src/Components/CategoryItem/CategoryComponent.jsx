6c1e2eee8eec8e4b52b51030d92c152ebc3c811e
Georgi Tsankov <145467172+GeorgiGeorgievTsankov@users.noreply.github.com>
2024-03-30 23:22:47 +0200

    Update CategoryComponent.jsx

--- src/Components/CategoryItem/CategoryComponent.jsx
+++ src/Components/CategoryItem/CategoryComponent.jsx
@@ -1,19 +1,21 @@
 import React from 'react'
 import './CategoryComponent.css'
+import { Link } from 'react-router-dom/cjs/react-router-dom'
 
 
 export const CategoryComponent = ({ category }) => {
-    const { imageUrl, title } = category;
+    const { imageUrl, title, route } = category;
 
     return (
         <div className="category-container">
             <div className="background-image" style={{ backgroundImage: `url(${imageUrl})` }}>
-                <div className="category-body-container">
-                    <h2>{title}</h2>
-                    <p>Shop now</p>
-                </div>
+                <Link to={route}>
+                    <div className="category-body-container">
+                        <h2>{title}</h2>
+                        <p>Shop now</p>
+                    </div>
+                </Link>
             </div>
         </div>
     )
 }
-
