94396ad8bdf435b78c85a507841faaf7a7a74326
Georgi Tsankov <145467172+GeorgiGeorgievTsankov@users.noreply.github.com>
2024-03-30 22:47:57 +0200

    Update CategoryComponent.jsx

--- src/Routes/Category/CategoryComponent.jsx
+++ src/Routes/Category/CategoryComponent.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useContext,useEffect,useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { CategoriesContext } from '../../Contexts/ProductsContext';
 import { useParams } from 'react-router-dom/cjs/react-router-dom';
 import "./CategoryStyle.css"
@@ -7,17 +7,21 @@ import ProductCardComponent from '../../Components/ProductCard/ProductCardCompon
 
 export const CategoryComponent = () => {
 
-  const {category} = useParams();
-  const {categoriesMap} = useContext(CategoriesContext)
- const [products, setProducts] = useState(categoriesMap[category]);
+  const { category } = useParams();
+  const { categoriesMap } = useContext(CategoriesContext)
+  const [products, setProducts] = useState(categoriesMap[category]);
 
- useEffect(() => {
-  setProducts(categoriesMap[category]);
- }, [categoriesMap, category])
+  useEffect(() => {
+    setProducts(categoriesMap[category]);
+  }, [categoriesMap, category])
 
   return (
-  <div className='category-containers'>
-     {products && products.map((product) => <ProductCardComponent key={product.id} product={product} />)}
-  </div>
+    <>
+      <h2 className='category-title'>{category.toUpperCase()}</h2>
+      <div className='category-containers'>
+        {products && products.map((product) => <ProductCardComponent key={product.id} product={product} />)}
+      </div>
+    </>
+
   )
 }
