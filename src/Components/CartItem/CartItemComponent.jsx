64506df1b37a5f0884ce27dca89243c6143bdede
Georgi Tsankov <145467172+GeorgiGeorgievTsankov@users.noreply.github.com>
2024-05-26 15:36:38 +0300

    Update CartItemComponent.jsx

--- src/Components/CartItem/CartItemComponent.jsx
+++ src/Components/CartItem/CartItemComponent.jsx
@@ -5,7 +5,7 @@ export const CartItemComponent = ({ cartItem }) => {
     const { name, quantity, price, imageUrl } = cartItem;
     return (
         <div className='cart-item-container'>
-            <img src={imageUrl} alt="product-image" />
+            <img src={imageUrl} alt="product" />
             <div className='item-details'>
                 <span className='name'>{name}</span>
                 <span className='price'>{quantity} x ${price}</span>
